a66589ce1bad41aa9c6ffdf1afacd9e419f1dab5
Marco Damaceno <maadmaaceno@gmail.com>
2016-06-28 13:33:19 -0300

    Tests

---
+++ app/validations/damauth/session_validations.rb
@@ -0,0 +1,21 @@
+module Damauth
+  class SessionValidations
+    def schema
+      Dry::Validation.Schema do
+        configure { config.messages = :i18n }
+        key(:email).required(:str?) do
+          format? EMAIL_REGEX
+        end
+        key(:password).required(:str?)
+      end
+    end
+
+    def validate(params = {})
+      schema.call(params)
+    end
+
+    def check_if_logged
+      true unless session[:user_id].nil?
+    end
+  end
+end
---
+++ spec/controllers/damauth/admin/sessions_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+module Damauth
+  module Admin
+    describe SessionsController, type: :controller do
+      routes { Damauth::Engine.routes }
+
+      let(:user) { create(:user, password: '123456', password_confirmation: '123456') }
+
+      describe 'GET #new' do
+        before do
+          get :new
+        end
+
+        check_response_with_template('new', 'damauth/application')
+
+        it 'redirects user if already logged' do
+          session[:user_id] = user.id
+          get :new
+          expect(response).to redirect_to '/'
+        end
+      end
+
+      describe 'POST #create' do
+        it 'login' do
+          post :create, user: { email: user.email, password: '123456' }
+          expect(session[:user_id]).to eq(user.id)
+        end
+
+        it 'redirects to the home#index (homepage)' do
+          post :create, user: { email: user.email, password: '123456' }
+          expect(response).to redirect_to '/'
+        end
+      end
+
+      describe 'GET #destroy' do
+        before do
+          session[:user_id] = user.id
+        end
+
+        it 'logout set user_id session to nil' do
+          get :destroy
+          expect(session[:user_id]).to_not eq(user.id)
+          expect(session[:user_id]).to be_nil
+        end
+
+        it 'logout redirects to homepage' do
+          get :destroy
+          expect(response).to redirect_to '/login'
+        end
+      end
+    end
+  end
+end
